6d52cfe41afb4f7a772a032ff9e161b381ae52c3
Daniel Castillo <danielcastillo89@icloud.com>
2023-05-31 22:50:45 -0500

    working on tests

--- frontend/components/App.test.js
+++ frontend/components/App.test.js
@@ -1,4 +1,38 @@
 // Write your tests here
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import { fireEvent } from "@testing-library/react";
+
+import AppFunctional from "./AppFunctional"
+
 test('sanity', () => {
   expect(true).toBe(false)
-})
+});
+
+test("renders without errors", () => {
+  render(<AppFunctional />);
+
+});
+
+test("", () => {
+  render(<AppFunctional />)
+
+});
+
+test("", () => {
+  render(<AppFunctional />)
+
+
+});
+
+test("", () => {
+  render(<AppFunctional />)
+
+
+});
+
+test("", () => {
+  render(<AppFunctional />)
+
+
+});
